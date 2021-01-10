3a01e09af4f684a3dd3a78909916a2fe8e8bc390
katcci <dubielka@student.agh.edu.pl>
2021-01-10 19:59:08 +0100

    added procfile etc

--- libraryproj/settings.py
+++ libraryproj/settings.py
@@ -114,8 +114,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_ROOT = 
+import django_heroku
+
+django_heroku.settings(locals())
